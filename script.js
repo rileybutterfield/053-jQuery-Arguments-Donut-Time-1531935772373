2bd085571428e01f54252e22ed1c1818c1d8a013
Riley Butterfield <41082883+rileybutterfield@users.noreply.github.com>
2018-07-18 13:42:58 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,14 @@
+$("#yes").click(function(){
+  $("#response").text("mmmm...");
+});
+
+$("#no").click(function(){
+    $("#response").text("d'oh!");
+});
+$("#donut").click(function(){
+    $("body").css("background-color","blue");
+});
+$("#donut").click(function(){
+    $("body").css("background-image","url(https://cdn1.benzinga.com/files/imagecache/1024x768xUP/images/story/2012/background-15226_640.jpg)");
+});
+
